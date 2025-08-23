2d13728ec4f2c1a5071af18ae0e2696bb366ac78
PLAlvarez11 <106171305+PLAlvarez11@users.noreply.github.com>
2025-08-23 01:01:55 -0600

    Limpieza por aca

--- frontend/marcador-baloncesto/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ScoreboardComponent } from './scoreboard/scoreboard';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: ScoreboardComponent
-  }
-];
--- frontend/marcador-baloncesto/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  apiUrl: 'http://localhost:5000/api' // Ajusta el puerto según tu backend
-};
-
