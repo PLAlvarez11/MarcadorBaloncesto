ad99e94aa4829505a0b2a974dc6518f908da2dd6
Daniel Miranda <dmirandac5@miumg.edu.gt>
2025-08-22 23:43:12 -0600

    Creacion de marcador

--- frontend/marcador-baloncesto/src/app/app.routes.ts
+++ frontend/marcador-baloncesto/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ScoreboardComponent } from './scoreboard/scoreboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ScoreboardComponent
+  }
+];
